1043b1bc5bf0b0c80805d3a283703b7322bfe683
Moein13r <mrahmat1386@gmail.com>
2022-07-05 17:39:46 +0430

    Configured Ocelot Api GetWay and Products  DownStreams and UP Streams

---
+++ ApiGetway/OcelotApiGetWay/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OcelotApiGetWay.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+
+    }
+}
